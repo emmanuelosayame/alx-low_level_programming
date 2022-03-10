e1c21c19d0b746b182867ad523f841cd56dffb14
bombl3bee <iamosayame@gmail.com>
2022-03-10 05:05:16 -0500

    file many

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+     #include <stdio.h>
 /**
  * main - main block
  * Return: 0
  */
+
+
+
+
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
